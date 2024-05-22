98b854c3050ee897658fc1bbea166b10ea44b761
solocoder9 <solocoder9@gmail.com>
2024-05-22 12:51:22 +0530

    modify

--- 01. Basic Programs/Calculator.java
+++ 01. Basic Programs/Calculator.java
@@ -1,51 +1,56 @@
+// Java calculator program
+
 import java.util.Scanner;
 
 public class Calculator {
-    public static void cal(int a, int b, char ch) {
-        int result;
 
-        switch(ch) {
-            case '+' : {
-                result = a + b;
-                System.out.println(result);
+    // Method to perform arithmetic operations on two numbers
+    public static void performCalculation(int a, int b, char operator) {
+        switch (operator) {
+            case '+':
+                System.out.println("Sum of a and b: " + (a + b));
                 break;
-            }
-            case '-' : {
-                result = a - b;
-                System.out.println(result);
+            case '-':
+                System.out.println("Subtraction of a and b: " + (a - b));
                 break;
-            }
-            case '*' : {
-                result = a * b;
-                System.out.println(result);
+            case '*':
+                System.out.println("Multiplication of a and b: " + (a * b));
                 break;
-            }
-            case '/' : {
-                result = a + b;
-                System.out.println(result);
+            case '/':
+                if (b != 0) {
+                    System.out.println("Division of a and b: " + (a / b));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
                 break;
-            }
-            default : {
-                System.out.println("Invalid input");
-            }
+            case '%':
+                if (b != 0) {
+                    System.out.println("Modulus of a and b: " + (a % b));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator. Please use one of +, -, *, /, %.");
+                break;
         }
     }
 
+    // Driver code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the value a: ");
+        System.out.print("Enter the value of a: ");
         int a = sc.nextInt();
 
         System.out.print("Enter the value of b: ");
         int b = sc.nextInt();
 
-        System.out.print("Enter operator between +, -, *, / : ");
-        char ch = sc.next().charAt(0);
+        System.out.print("Enter the operator: ");
+        char operator = sc.next().charAt(0);
 
-        cal(a, b, ch);
+        performCalculation(a, b, operator);
 
         sc.close();
-
     }
 }
